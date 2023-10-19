bc8e4244abe8cb8c9cddf442ec7386b15f649ef5
ahmedafrotoo <148379018+ahmedafrotoo@users.noreply.github.com>
2023-10-19 21:26:28 +0300

    Update session.py

--- Afrotoo/core/session.py
+++ Afrotoo/core/session.py
@@ -4,7 +4,7 @@ from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 from telethon.sessions import StringSession
 from telethon.errors import AccessTokenExpiredError, AccessTokenInvalidError
 from ..Config import Config
-from .Alafrotoo import AlafrdPyro
+from .Afrotoo import AlafrdPyro
 from .client import mainClient
 LOGS = logging.getLogger(" ")
 
